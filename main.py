9ca17d833388b47acd8b13c03874f7b09c4d6833
InobatTagaymurodovna <86709798+InobatTagaymurodovna@users.noreply.github.com>
2021-07-02 08:59:59 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,161 @@
+a=1; b=2; print(a,b) #
+"""x="shirin"
+def funksiya():
+     x="foydali"
+    print("olma"+x)
+funksiya()
+print("olma " +x)"""
+'''print("A nuqtani qiymatini kiriting")
+A=int(input())
+print("B nuqtani qiymatini kiriting")
+B=int(input())
+print("C nuqtani qiymatini kiriting")
+C=int(input())
+k=abs(C-A)
+d=abs(B-C)
+print(k)
+print(d)
+print(k+d)'''
+'''x=-29
+a=x//-4
+print(a)'''
+
+'''print(a>4) or (a<4)'''
+'''x=["olma","banan"]
+y=["olma","banan"]
+print(x is y)'''
+s='spam'''
+'''a=50
+b=30
+if a<b:
+    print("a soni b sonidan katta")
+elif a==b:
+    print("ular o`zaro teng")
+elif a<b:
+    print("a soni b sonidan kichik")
+    pass
+else:
+    print("hech qaysi shart bajarilmadi")'''
+i=0
+'''while i<11:
+    i+=1
+    if (i==6) or (i==8):
+        continue
+    print(i)'''
+'''i=5
+while i<11:
+    print(i)
+    if i:
+else:
+print("sikl toxtadi")'''
+'''a="dastur"
+for a in a:
+    if a=="s":
+        continue
+    print(a)'''
+'''for x in range(5):
+    print("python")
+else:
+    print("\nsikl tugadi")'''
+a=["qora","oq","qizil"]
+b=["spark","Nexia", "Lasetti"]
+'''for x in a:
+    for y in b:
+        print(x,y)'''
+'''for x in range(10):
+    print(str(x)*x)'''
+'''a=10
+a=int(input())
+for i in range(1,1+a):
+    for j in range(1,1+a):
+        print(i,"x",j,"=",i*j)'''
+'''k=int(input("k="))
+n=int(input("n="))
+for i in n:
+    print(k)'''
+'''meva=["olma","anor","apelsin"]
+meva[0]="anor"
+print(meva)'''
+'''mashina=["Audi","Mustang", "Ferrari", "Nexsiya"]
+for x in mashina:
+    print(mashina)'''
+'''meva=["olma","banan", "apelsin","nok"]
+if "shaftoli" in meva:
+    print("ha, nok bor")
+else:
+    print("nok yo'q")'''
+'''meva=["olma","anor","apelsin"]
+meva.append("shaftoli")
+print(meva)'''
+'''meva=["olma","anor","apelsin"]
+print(len(meva))'''
+'''meva=["olma","anor","apelsin"]
+L=["shashlik", "somsa"]
+meva.extend(meva)
+print(meva)'''
+'''meva=["olma","anor","apelsin"]
+i=3
+x="nok"
+meva.insert(i,x)
+print(meva)'''
+'''meva=["olma","anor","apelsin"]
+i=2
+meva.pop(i)
+print(meva)'''
+'''a={9,4,5,2,1}
+b={"fg","yt","iuj",5,1}
+c=a.issuperset(b)
+print(c)'''
+'''a={9,4,5,2,1}
+b={"fg","yt","iuj",5,1}
+b.symmetric_difference_update(a)
+print(b)'''
+'''a=(11,22)
+b=(99,88)
+a=b
+b=(11,22)
+print(a)
+print(b)
+
+d=oct(15)
+print(d)
+keys=['Ten','Twenty','Thirty']
+values=[10,20,30]
+for x,y in zip(keys,values[::]):
+    print(x,y)
+a=dict(ten="10",Twenty="20", Thirty="30" )
+print(a)
+a={'Ten':10,'Twenty':20,'Thirty':30}
+b={'Thirty':30,'Fourty':40,'Fifty':50}
+import random
+a=[6,8,9]
+print(random.choice(a))
+import random
+a='6hjhg9'
+print(a[5:3:-1])
+a="ol Python 13 **dasturlash\n python tili"
+k=a.startwith("ol")
+print(k)'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
